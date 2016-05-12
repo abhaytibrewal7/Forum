e1d6d8ff19b0e3bd601cab8599763d90681638bd
abhaytibrewal7 <abhaytibrewal7@gmail.com>
2016-05-12 13:52:30 +0530

    Commiting untracked files

---
+++ src/Controller/Post.java
@@ -0,0 +1,98 @@
+package Controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.forum_name;
+
+@WebServlet("/view_post_page")
+public class Post extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		HttpSession session = req.getSession();	
+		int forum_id = Integer.valueOf(req.getParameter("forum_id"));
+		String forumName = "";
+	
+		if(session.getAttribute("forum") != null){
+			ArrayList<forum_name> forum = (ArrayList<forum_name>)session.getAttribute("forum");
+			ListIterator forum_li = forum.listIterator();
+
+			while(forum_li.hasNext()){
+			
+				forum_name f = new forum_name();
+				f = (forum_name)forum_li.next();
+				
+				int id = f.getId();								
+				if(id == forum_id){
+					forumName = f.getName();
+				}
+			}
+		}
+			String html="<div class='container'>"+				
+			"<div class='row top-buffer'>"+
+				"<div id='main-body'>"+
+					"<div class='col-md-12 white'>"+
+						"<div class='row'>"+
+							"<div class='col-md-12'>"+
+								"<h3>Post A Question</h3>"+
+							"</div>"+
+						"</div>"+
+						"<div class='row'><p>"+
+							"<div class='col-md-7 col-md-offset-1'>"+
+								"<form method='post' action='uploadServlet' enctype='multipart/form-data'>"+
+									"<input type='hidden' name='forum_id' value='"+forum_id+"'>"+
+									"<div class='form-group row'>"+
+										"<label for='inputEmail3' class='col-sm-2 form-control-label'>Forum</label>"+
+										"<div class='col-sm-10'>"+
+											"<input name='forum_name' type='text' class='form-control' id='inputEmail3' value='"+forumName+"'>"+
+										"</div>"+
+									"</div>"+
+									"<div class='form-group row'>"+
+										"<label for='inputPassword3' class='col-sm-2 form-control-label'>Question</label>"+
+										"<div class='col-sm-10'>"+
+											"<textarea name='question' class='form-control' rows='10' id='question'></textarea>"+
+											"<br>"+
+										"</div>"+
+									"</div>"+
+									"<div class='form-group row'>"+
+										"<label for='attachment' class='col-sm-2 form-control-label'>Choose Attachment:</label>"+
+										"<div class='col-sm-10'>"+
+											"<input type='file' name='attachment' size='50' id='attachment' />"+
+											"<br>"+
+										"</div>"+
+									"</div>"+
+									"<div class='form-group row pull-right'>"+
+										"<input type='submit' class='btn green' value='Submit'>&nbsp;&nbsp;&nbsp;&nbsp;"+
+										"<a href='view_threads.html?id="+forum_id+"' class='btn green'>"+
+											"<span class='glyphicon glyphicon-arrow-left' aria-hidder='true'></span>Cancel"+
+										"</a>"+
+									"</div>"+
+								"</form>"+
+							"</div>"+
+						"</div>"+
+					"</div>"+
+				"</div>"+
+			"</div>"+
+		"</div>"+
+		"<footer class='footer'>"+
+			"<div class='container text-center'>"+
+				"<p style='margin-top: 20px;'>Powered by <h4>Aunwesha Pvt Ltd</h4></p>"+
+			"</div>"+
+		"</footer>"+
+	"</body>"+
+	"</html>";
+			
+			resp.getWriter().write(html);
+	}
+}
---
+++ src/Controller/ViewThreadMsgs.java
@@ -0,0 +1,200 @@
+package Controller;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.forum_name;
+import model.thread_messages;
+import model.threads;
+
+@WebServlet("/view_thread_msg_page")
+public class ViewThreadMsgs extends HttpServlet {	
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		HttpSession session = req.getSession();	
+		int forum_id = Integer.valueOf(req.getParameter("forum_id"));
+		int thread_id = Integer.valueOf(req.getParameter("thread_id"));
+			
+		ListIterator<forum_name> forum_li = null;
+		ListIterator<threads> thread = null;
+		ListIterator<thread_messages> thread_messages = null;
+		String forum_name = "", html = "", html2 = "", thread_name = "";
+		if(session.getAttribute("forum") != null){
+			ArrayList<forum_name> forum = (ArrayList<forum_name>)session.getAttribute("forum");
+			forum_li = forum.listIterator();
+		}
+		if(session.getAttribute("thread") != null){
+			ArrayList<threads> t = (ArrayList<threads>)session.getAttribute("thread");
+			thread = t.listIterator();
+		}
+		if(session.getAttribute("thread_messages") != null){
+			ArrayList<thread_messages> t_m = (ArrayList<thread_messages>)session.getAttribute("thread_messages");
+			thread_messages = t_m.listIterator();
+		}
+		if(session.getAttribute("forum") != null){							
+			while(forum_li.hasNext()){
+								
+				forum_name f = new forum_name();
+				f = (forum_name)forum_li.next();
+									
+				int id = f.getId();								
+				if(id == forum_id){
+					forum_name=f.getName();
+				}
+			}
+		}
+			
+		html = "<div class='container'>"+
+				"<div id='main-body'>"+
+					"<div id='title'>"+
+						"<div class='row top-buffer'>"+
+							"<h3>"+forum_name+"</h3>"+
+						"</div>"+
+					"</div>"+
+					"<div id='post-nav'>"+
+						"<div class='row top-buffer'>"+
+							"<div class='col-md-2'>"+
+								"<a href='view_threads.html?id="+forum_id+"' class='btn green'>"+
+									"<span class='glyphicon glyphicon-arrow-left' aria-hidder='true'></span>&nbsp;&nbsp;Go Back"+
+								"</a>"+
+							"</div>"+										
+						"</div>"+
+					"</div>"+
+					"<div id='post-body'>"+
+						"<div class='row top-buffer'>"+
+							"<div class='col-md-9 light-gray'>"+
+								"<div id='post-title'>"+
+									"<div class='row'>"+
+										"<div class='col-md-7' style='font-weight:bold'>Question :";
+	
+										int notOfMsgs = 0;
+										Timestamp noOfPosts = null;
+										while(thread.hasNext()) {
+							
+												threads t = (threads)thread.next();
+												if(t.getId() == thread_id){
+													thread_name = t.getName();
+													int user_id = t.getAuthor();
+													notOfMsgs = t.getNo_of_messages();
+													noOfPosts = t.getLast_post();
+	
+													html2 += thread_name+"<br/>"+
+															"<script>"+
+																"getUser("+user_id+","+thread_id+");"+
+															"</script>"+
+															"<br/>"+												
+															"<span style='color: #AA0000;' class='username-coloured' id='user_name"+thread_id+"'></span>";
+							}										
+						}											
+					html2 += "</div>"+								
+							"</div>"+
+						"</div>"+
+						"<hr>"+
+						"</div>"+
+						"<div class='col-md-3 light-dark-gray'>"+
+						"<br/>"+
+						"<dl class='postprofile' id='profile4'>"+							
+							"<dd><strong>Posts:</strong>"+notOfMsgs+"</dd>"+
+							"<dd><strong>Last Post:</strong> "+noOfPosts+"</dd>"+
+							"<hr>"+							
+						"</dl>"+
+					"</div>"
+					+ "</div>"+
+				"</div>";
+					while(thread_messages.hasNext()){
+						
+						thread_messages t = (thread_messages)thread_messages.next();
+	
+	
+						int user_id = t.getPosted_by(); 
+						int thread_msg_id = t.getId();
+					
+				
+						html2 += "<div id='post-body'>"+
+							"<div class='row top-buffer'>"+
+								"<div class='col-md-9 white'>"+
+									"<div id='post-title'>"+
+										"<div class='row'>"+																
+											"<div class='col-md-offset-7 col-md-4'>"+
+												"<div id='post-opts'>"+
+													"<ul class='post-options text-right'>"+											
+														"<li><span class='glyphicon glyphicon-text-color' aria-hidden='true'></span>&nbsp;"+
+													
+															"<a href='#' >Reply</a>"+
+														"</li>"+
+													"</ul>"+
+												"</div>"+
+											"</div>"+
+										"</div>"+
+									"</div>"+					
+									"<div id='post-content'>"+
+										"<div class='row'>"+
+											"<div class='col-md-12'>"+
+												"<b>Reply : </b>"+t.getMessage_content()+
+												"<br/><br/>"+
+												"<div class='col-md-offset-9'>"+
+													"<span class='glyphicon glyphicon-file' aria-hidden='true'></span>&nbsp;"+t.getPosting_date()+
+												"</div>"+		
+											"</div>"+
+										"</div>"+
+									"</div>"+
+								"</div>"+
+								"<div class='col-md-3 light-dark-gray'>"+
+									"<br/>"+
+									"<dl class='postprofile' id='profile4'>"+
+										"<dt>"+
+											"<script>"+
+												"getUser("+user_id+","+thread_msg_id+");"+
+											"</script>"+
+											"<a href='#' style='color: #AA0000;' class='username-coloured' id='user_name"+thread_msg_id+"'></a>"+
+										"</dt>"+																					
+									"</dl>"+
+								"</div>"+
+							"</div>"+
+						"</div>";			
+					}
+				
+				html2 += "<div id='post-body'>"+
+							"<div class='row top-buffer'>"+
+								"<div class='col-md-9 light-gray'>"+
+									"<div class='form-group'>"+
+										"<label for='replyQuestion'>Reply:</label>"+
+											"<textarea class='form-control' rows='5' placeholder='Reply to this Question!' id='replyQuestion'></textarea>"+
+									"</div>"+
+								"</div>"+					
+							"<br/>"+
+							"</div>"+
+							"<div class='row top-buffer'>"+
+								"<div class='col-md-4'>"+
+									"<button type='button' class='btn green' onclick='replySubmit("+thread_id+","+forum_id+")'>Submit</button>&nbsp;&nbsp;&nbsp;"+
+									"<a href='view_threads.html?id="+forum_id+"' class='btn green'>"+
+										"<span class='glyphicon glyphicon-arrow-left' aria-hidder='true'></span>Cancel"+
+									"</a>"+
+								"</div>"+
+						"</div>"+									
+					"</div>"+
+				"</div>"+	
+		"</div>"+
+		"<footer class='footer'>"+
+			"<div class='container text-center'>"+
+				"<p style='margin-top: 20px;'>Powered by <h4>Aunwesha Pvt Ltd</h4></p>"+
+			"</div>"+
+		"</footer>"+
+	"</body>"+
+	"</html>";	
+				
+	resp.getWriter().write(html+html2);			
+	}
+}
